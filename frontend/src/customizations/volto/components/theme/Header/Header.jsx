35cd3a9c41ebc467d5c549e7285bc27ce59333ae
Rafahela <rafahela@gmail.com>
2022-11-22 16:49:03 -0300

    Implementação do cabeçalho.

---
+++ frontend/src/customizations/volto/components/theme/Header/Header.jsx
@@ -0,0 +1,110 @@
+/**
+ * Header component.
+ * @module components/theme/Header/Header
+ */
+
+ import React, { Component } from 'react';
+ import { Container, Segment } from 'semantic-ui-react';
+ import PropTypes from 'prop-types';
+ import { connect } from 'react-redux';
+ 
+ import {
+   Anontools,
+   LanguageSelector,
+   Logo,
+   Navigation,
+   SearchWidget,
+ } from '@plone/volto/components';
+ 
+ /**
+  * Header component class.
+  * @class Header
+  * @extends Component
+  */
+ class Header extends Component {
+   /**
+    * Property types.
+    * @property {Object} propTypes Property types.
+    * @static
+    */
+   static propTypes = {
+     token: PropTypes.string,
+     pathname: PropTypes.string.isRequired,
+   };
+ 
+   /**
+    * Default properties.
+    * @property {Object} defaultProps Default properties.
+    * @static
+    */
+   static defaultProps = {
+     token: null,
+   };
+ 
+   /**
+    * Render method.
+    * @method render
+    * @returns {string} Markup for the component.
+    */
+   render() {
+     return (
+<header>
+  <nav class="Triad navbar_global">
+    <div>
+      <div class="u-hideLower">
+        <div class="Rail Rail--fenced"><a class="link link-deep" href="#view" accesskey="1">Ir para o conteúdo principal</a><a class="link link-deep" href="#navigation" accesskey="2">Menu</a><a class="link link-deep" href="#footer" accesskey="3">Rodapé</a></div>
+      </div>
+    </div>
+    <div>
+      <div class="u-hideUpper"><a class="navbar-icons btn btn-secondary" href="#search" title="Buscar"><i class="fas fa-search"></i></a></div>
+      <div class="u-hideLower">
+        <ul class="flex-unstyled li-spacer">
+          <li><a class="flex-item--center link link-deep" href="#" title="Fonte maior">A+</a></li>
+          <li><a class="flex-item--center link link-deep" href="#" title="Fonte menor">A-</a></li>
+          <li><a class="flex-item--center link link-deep" href="acessibilidade" title="Acessibilidade">Acessibilidade</a></li>
+          <li class="fence--solo"></li>
+          <li><a class="flex-item--center link link-deep" href="#" title="Contraste">Contraste</a></li>
+          <li class="fence--solo"></li>
+          <li><a class="flex-item--center link link-deep js-vlibras" href="#" role="button" title="VLibras">VLibras</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ 
+  <div class="u-hideLower">
+    <nav class="navbar_control navbar_control--tertiary navbar_control--spacer" id="principal">
+      <div class="control_item">
+        <Navigation pathname={this.props.pathname} />    
+      </div>
+      <div class="control_item control_item--3 justify-content-center">
+       {/*<ul class="fence fence--xl fence-border--lg fence-color--cousin">
+          <li><a class="link link-deep--facebook" href="https://www.facebook.com/interlegisonline/"><i class="fab fa-facebook"></i></a></li>
+          <li><a class="link link-deep--instagram" href="https://www.instagram.com/interlegis_/"><i class="fab fa-instagram"></i></a></li>
+          <li><a class="link link-deep--youtube" href="https://www.youtube.com/channel/UCggZpGPjK5eF1vWoONIqIbw/"><i class="fab fa-youtube"></i></a></li>
+     </ul>*/}
+      </div>
+      <div className="tools-search-wrapper">
+        <div className="search">
+          <SearchWidget />
+        </div>
+     </div>
+    </nav>
+  </div>
+  <Container>
+    <div className="header">
+      <div className="logo-nav-wrapper">
+        <div className="logo">
+          <Logo />
+          <p>Portal da Comunidade Interlegis de Tecnologia</p>
+        </div>
+      </div>
+    </div>
+  </Container>
+</header>
+     );
+   }
+ }
+ 
+ export default connect((state) => ({
+   token: state.userSession.token,
+ }))(Header);
